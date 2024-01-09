3426e9e241d01e59ac21adf97eaedec2ac63f115
Hamlet Marekhashvili <hamlet@bodidata.com>
2024-01-09 16:33:55 +0100

    Added additional pages for the navigation menu

---
+++ src/app/page2/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div className="m-8">Page 2</div>;
+};
+
+export default Page;
---
+++ src/app/page3/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div className="m-8">Page 3</div>;
+};
+
+export default Page;
