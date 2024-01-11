b54e2c1256079574d8baf78aa033f71fff875200
Hamlet Marekhashvili <hamlet@bodidata.com>
2024-01-11 12:46:25 +0100

    Added basic stopwatch timer implementation

--- src/app/timer_apps/page.tsx
+++ src/app/timer_apps/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { default as Stopwatch } from "./stopwatch";
+
 const Page = () => {
   return (
     <div>
+      {" "}
+      <Stopwatch />
     </div>
   );
 };
---
+++ src/app/timer_apps/stopwatch.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Stopwatch() {
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimer((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return <div className="m-8">Current timer is at {timer}</div>;
+}
