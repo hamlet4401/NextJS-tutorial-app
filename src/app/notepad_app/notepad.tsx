7e952ca776e8e2588ccd5a4f0aaf973c81bdf2c3
Hamlet Marekhashvili <hamlet@bodidata.com>
2024-01-17 15:24:24 +0100

    Updated notepad app with file upload and download

--- src/app/notepad_app/notepad.tsx
+++ src/app/notepad_app/notepad.tsx
@@ -1,21 +1,38 @@
-import React from "react";
+"use client";
+import React, { ChangeEvent } from "react";
+import { default as useNotepadStore } from "../store/notepadStore";
 
 const Notepad = () => {
+  const { title, setTitle, noteText, setNoteText } = useNotepadStore();
+  const handleNoteTextValue = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setNoteText(event.target.value);
+  };
+
+  const handleTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
   return (
-    <div className="size-full border-2">
-      <div className="flex flex-col items-center size-full border-2">
-        <div className="flex flex-col items-center justify-center size-full border-2 ">
+    <div className="size-full">
+      <div className="flex flex-col items-center size-full">
+        <div className="flex flex-col items-center justify-center w-full h-1/2">
           <div className="w-1/2 mb-2">
+            <label className="text-xs">File name</label>
             <input
               type="text"
               placeholder="Title"
               className="input input-bordered input-primary w-full"
+              value={title}
+              onChange={handleTitle}
             />
           </div>
-          <div className="flex justify-center items-center size-1/2 border-2">
+          <div className="justify-center items-center size-1/2">
+            <label className="text-xs">Content</label>
             <textarea
               className="textarea textarea-primary size-full"
-              placeholder="Bio"
+              placeholder="Text"
+              onChange={handleNoteTextValue}
+              value={noteText}
             ></textarea>
           </div>
         </div>
--- src/app/notepad_app/sideSelectionMenu.tsx
+++ src/app/notepad_app/sideSelectionMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React from "react";
 import { default as SideMenuButtons } from "../components/side_menu/sideMenuButton";
+import { default as SideMenuDropdown } from "../components/side_menu/sideMenuDropdown";
+import { default as useNotepadStore } from "../store/notepadStore";
 
 import fileAddLightTheme from "../../../public/light_mode/file-plus.svg";
 import fileAddDarkTheme from "../../../public/dark_mode/file-plus.svg";
@@ -10,35 +11,94 @@ import uploadDarkTheme from "../../../public/dark_mode/upload.svg";
 
 import saveLightTheme from "../../../public/light_mode/save.svg";
 import saveDarkTheme from "../../../public/dark_mode/save.svg";
+import { ChangeEvent, useRef } from "react";
 
 const SideSelectionMenu = () => {
+  const { setTitle, setNoteText, noteText, title } = useNotepadStore();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleNewPage = () => {
+    setTitle("NewFile.txt");
+    setNoteText("");
+  };
+
+  const handleFileUploadClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleNoteTextValue = (changeEvent: ChangeEvent<HTMLInputElement>) => {
+    const fileInput = changeEvent.target;
+
+    if (fileInput.files && fileInput.files.length > 0) {
+      const selectedFile = fileInput.files[0];
+      const fileName = selectedFile.name;
+      setTitle(fileName);
+
+      const fileReader = new FileReader();
+
+      fileReader.onload = (progressEvent: ProgressEvent<FileReader>) => {
+        const fileContent = progressEvent.target?.result as string;
+        setNoteText(fileContent);
+      };
+      fileReader.readAsText(selectedFile);
+    }
+  };
+
+  const handleFileSave = () => {
+    if (noteText) {
+      const blob = new Blob([noteText], { type: "text/plain;charset=utf-8" });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = title;
+      a.style.display = "none";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    }
+  };
+
   return (
-    <div className="">
+    <div className="size-full">
       <div className="flex-col mb-10">
         <SideMenuButtons
           iconSourceDarkMode={fileAddDarkTheme}
           iconSourceLightMode={fileAddLightTheme}
           alternativeIconText="Add icon"
-          onClickEvent={() => {}}
+          onClickEvent={handleNewPage}
         >
-          Create new file
+          New file
         </SideMenuButtons>
-        <SideMenuButtons
-          iconSourceDarkMode={uploadDarkTheme}
-          iconSourceLightMode={uploadLightTheme}
-          alternativeIconText="Upload icon"
-          onClickEvent={() => {}}
-        >
-          Upload and modify file
-        </SideMenuButtons>
+        <div className="flex flex-col">
+          <SideMenuButtons
+            iconSourceDarkMode={uploadDarkTheme}
+            iconSourceLightMode={uploadLightTheme}
+            alternativeIconText="Upload icon"
+            onClickEvent={handleFileUploadClick}
+          >
+            Upload
+          </SideMenuButtons>
+          <div className="justify-center pl-8 pr-4 mt-1">
+            <input
+              type="file"
+              ref={fileInputRef}
+              className="file-input file-input-primary file-input-xs max-w-xs"
+              accept=".txt"
+              onChange={handleNoteTextValue}
+            />
+          </div>
+        </div>
       </div>
       <SideMenuButtons
         iconSourceDarkMode={saveDarkTheme}
         iconSourceLightMode={saveLightTheme}
         alternativeIconText="Save icon"
-        onClickEvent={() => {}}
+        onClickEvent={handleFileSave}
       >
-        Save file
+        Download
       </SideMenuButtons>
     </div>
   );
