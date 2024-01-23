df0b81e4ab5ff28a2776e79111734dc5d6ff2733
Hamlet Marekhashvili <hamlet@bodidata.com>
2024-01-23 12:39:29 +0100

    Added loading icon

---
+++ src/app/components/custom_logos/loadingAnimated.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const LoadingAnimated = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="size-full  animate-spin"
+      stroke="currentColor"
+      fill="none"
+      viewBox="0 0 100 100"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        stroke-width="10"
+        r="25"
+        stroke-dasharray="100 40"
+      ></circle>
+    </svg>
+  );
+};
+
+export default LoadingAnimated;
