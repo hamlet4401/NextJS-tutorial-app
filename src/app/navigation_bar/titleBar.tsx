6159737b3236f521d580b58e86b6e72b937e9c17
Hamlet Marekhashvili <hamlet@bodidata.com>
2024-01-23 12:41:18 +0100

    Added database menu item

--- src/app/navigation_bar/titleBar.tsx
+++ src/app/navigation_bar/titleBar.tsx
@@ -1,14 +1,13 @@
 import Link from "next/link";
 import React from "react";
 import { default as ThemeButton } from "./themeButton";
-import { default as LoginButton } from "../components/navigation_bar/loginButton";
 
 import BodidataLogo from "../components/custom_logos/bodidataLogo";
 
 const TitleBar = () => {
   return (
     <nav className="">
-      <div className="flex justify-between items-center border-b-2">
+      <div className="flex justify-between items-center border-b-2 border-current">
         <div className="flex space-x-6 items-center">
           <Link href="/">
             <div className="h-4">
@@ -22,10 +21,12 @@ const TitleBar = () => {
             <li>
               <Link href={"/timer_apps"}>Timer Apps</Link>
             </li>
+            <li>
+              <Link href={"/database"}>Database</Link>
+            </li>
           </ul>
         </div>
         <div className="flex space-x-6 items-center">
-          <LoginButton loginType="database" />
           <ThemeButton />
         </div>
       </div>
