85335da0a246e0a5897312207b91bb04d42eabef
Hamlet Marekhashvili <hamlet@bodidata.com>
2024-01-23 12:41:56 +0100

    Added database connection code

---
+++ src/app/database/databaseConnection.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export default async function DatabaseConnection(
+  username: string,
+  password: string
+) {
+  username = "hamletmarekhashvili";
+  password = "PQrcrR4yc4QYV3yX";
+
+  const uri =
+    "mongodb+srv://" +
+    username +
+    ":" +
+    password +
+    "@cluster0.1e3zxzq.mongodb.net/";
+
+  localStorage.setItem("databaseUri", uri);
+}
